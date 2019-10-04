0797e43781d3e0bb44b85c8516fd172d52b62824
meganjeffcoat <jeffcoat.megan@gmail.com>
2019-10-04 10:27:58 -0500

    ex1 complete and tests pass

--- hashtables/ex1/ex1.py
+++ hashtables/ex1/ex1.py
@@ -1,9 +1,7 @@
 #  Hint:  You may not need all of these.  Remove the unused functions.
 from hashtables import (HashTable,
                         hash_table_insert,
-                        hash_table_remove,
-                        hash_table_retrieve,
-                        hash_table_resize)
+                        hash_table_retrieve)
 
 
 def get_indices_of_item_weights(weights, length, limit):
@@ -12,6 +10,21 @@ def get_indices_of_item_weights(weights, length, limit):
     """
     YOUR CODE HERE
     """
+    # weight is the weights at i
+    # weight_limit is the limit - weight
+    # retrieve the weight_limit and set it as index_pair
+    # if index_pair is not None and higher than i it will be index_pair first, 
+    # else it will be i first
+        # return the group once we know what order it should goin
+    # insert everything into the hash table
+    for i in range(length):
+        weight = weights[i]
+        weight_limit = limit - weight
+        index_pair = hash_table_retrieve(ht, weight_limit)
+        if index_pair is not None:
+            group = (index_pair, i) if i < index_pair else(i, index_pair)
+            return group
+        hash_table_insert(ht, weight, i)
 
     return None
 
@@ -21,3 +34,6 @@ def print_answer(answer):
         print(str(answer[0] + " " + answer[1]))
     else:
         print("None")
+
+
+
